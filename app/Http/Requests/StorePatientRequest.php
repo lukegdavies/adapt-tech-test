35df7423b05b4fb2398ec7940993011a53f9aea6
Luke Davies <luke@fluffyegg.com>
2024-07-28 23:40:49 +0100

    Created a form Request for validation
    This includes all rules for the form create / update. Even included a method before it validates to trim anything before insertion

---
+++ app/Http/Requests/StorePatientRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePatientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $patientId = $this->patient ? $this->patient : null;
+
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => ['required', 'email', 'unique:patients,email,' . $patientId],
+            'phone_number' => 'nullable|digits_between:11,15',
+            'address' => 'required|string|max:300',
+            'nhs_number' => ['required', 'string', 'max:255', 'unique:patients,nhs_number,' . $patientId],
+            'date_of_birth' => 'required|date',
+            'sex' => 'required|in:M,F,O,P',
+        ];
+    }
+
+    protected function prepareForValidation() {
+        $this->merge([
+            'address' => preg_replace("/\s+/", " ", trim($this->address)),
+            'first_name' => trim($this->first_name),
+            'last_name' => trim($this->last_name),
+            'email' => strtolower(trim($this->email)),
+        ]);
+    }
+}
