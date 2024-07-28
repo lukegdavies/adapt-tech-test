18412c0957866381716faf7326499237418bbfa4
Luke Davies <luke@fluffyegg.com>
2024-07-28 22:27:15 +0100

    Created PatientController and added a route
    The routing is made from resource api flag

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\{ProfileController, PatientController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +10,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('patients', PatientController::class)->middleware('auth');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
