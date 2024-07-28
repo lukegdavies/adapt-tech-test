3676b338536ab954ee8af4b37bd03a9880a2a1af
Luke Davies <luke@fluffyegg.com>
2024-07-28 22:40:54 +0100

    Added font inter to guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,8 +8,9 @@
         <title>{{ config('app.name', 'Adapt Tech test') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&family=Inter:wght@100..900&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
