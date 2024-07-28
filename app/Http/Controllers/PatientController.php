ba41f96f051e47e0d31c55da756ba42877cf6408
Luke Davies <luke@fluffyegg.com>
2024-07-28 23:13:09 +0100

    Added a create Patient method for create form

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -22,6 +22,14 @@ class PatientController extends Controller
     }
 
     /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('patients.create');
+    }
+
+    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
