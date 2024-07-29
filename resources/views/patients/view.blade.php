fcaa3913066b6158883336ee4c7e6c33466bf185
Luke Davies <luke@fluffyegg.com>
2024-07-29 09:54:35 +0100

    Adding comments for view page

--- resources/views/patients/view.blade.php
+++ resources/views/patients/view.blade.php
@@ -1,15 +1,21 @@
 <x-app-layout>
 <div class="bg-white overflow-hidden shadow  border">
     <div class="px-4 py-5 sm:px-6">
+        <!-- Header for the user profile section -->
         <h3 class="text-lg leading-6 font-medium text-gray-900">
             User Profile
         </h3>
+         <!-- Subtext describing the section's content -->
         <p class="mt-1 max-w-2xl text-sm text-gray-500">
             This is some information about the Patient.
         </p>
     </div>
+    <!-- Detailed view of user profile information -->
     <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
         <dl class="sm:divide-y sm:divide-gray-200">
+            <!-- Repeatable block for each attribute of the patient, formatted in a grid layout -->
+
+            <!-- First Name -->
             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">
                     First name
@@ -20,6 +26,8 @@
                     @endif
                 </dd>
             </div>
+
+            <!-- Last Name -->
             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">
                     Last name
@@ -30,6 +38,8 @@
                     @endif
                 </dd>
             </div>
+
+            <!-- Email Address -->
             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">
                     Email address
@@ -40,6 +50,8 @@
                    @endif
                 </dd>
             </div>
+
+            <!-- Phone Number -->
             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">
                     Phone number
@@ -50,6 +62,8 @@
                     @endif
                 </dd>
             </div>
+
+            <!-- Address -->
             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">
                     Address
@@ -60,6 +74,8 @@
                     @endif
                 </dd>
             </div>
+
+            <!-- NHS number -->
             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">
                     NHS number
@@ -70,6 +86,8 @@
                     @endif
                 </dd>
             </div>
+
+            <!-- Date of birth -->
             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">
                     Date of birth
@@ -80,6 +98,8 @@
                     @endif
                 </dd>
             </div>
+
+            <!-- Sex / Gender -->
             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">
                     Sex/Gender
